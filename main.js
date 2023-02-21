7f624ac2b5d5935d8bf83f7687c29d9df6fe3667
Durbek <pattoyevd@gmail.com>
2023-02-21 19:47:41 +0500

    commit

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+//switch
 const name = "Durbek"
 
 switch(name) {
-    case "Durbek"
-    console.log("qalesiz") 
-    break; //shart tugadi
-    case
+    case "Durbek":
+      console.log("qalesiz") 
+      break; //shart tugadi
+    case "Durbek0222":
+      console.log("chiqmedi barbir")
+      break;
+    default:
+      console.log("chiqmedi buyam")
+      break;
 }
